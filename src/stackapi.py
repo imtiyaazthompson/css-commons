992423153a7b9369f5b30338fe926c64235437b0
imtiyaazthompson <imtiyaaztom@gmail.com>
2021-03-05 12:03:32 +0200

    Obtained all FAQ for CSS, attempt to get body of the Q and A. All Details are stored in the faq.json

--- src/stackapi.py
+++ src/stackapi.py
@@ -9,11 +9,13 @@ API_KEY = config.api_key
 SECRET = config.secret_key
 API = "https://api.stackexchange.com/2.2/"
 rparams = {
+    "key": API_KEY,
     "site": "stackoverflow",
-    "page": 1,
+    "page": 197,
     "pagesize": 100,
 }
 
+# Hold all details from a call to the API
 faq = []
 
 
@@ -43,19 +45,44 @@ def get_tag_faq(tag):
     request_url = API + "tags/" + tag + "/" + sk.search["faq"]
     response = get(request_url)
 
-    #show_keys(response)
-
+    # Extract Data
+    # Provide checks for a key that may not be present in response
     for item in response["items"]:
-        faq.append({
-            "tags": item["tags"],
-            "is_answered": item["is_answered"],
-            "score": item["score"],
-            "question_id": item["question_id"],
-            "answer_id": item["accepted_answer_id"],
-            "title": item["title"],
-            "link": item["link"],
-            "last_updated": item["last_edit_date"]
-        })
+        detail = {}
+        if "tags" in item.keys():
+            detail["tags"] = item["tags"]
+        else:
+            detail["tags"] = None
+        if "is_answered" in item.keys():
+            detail["is_answered"] = item["is_answered"]
+        else:
+            detail["is_answered"] = None
+        if "score" in item.keys():
+            detail["score"] = item["score"]
+        else:
+            detail["score"] = None
+        if "question_id" in item.keys():
+            detail["question_id"] = item["question_id"]
+        else:
+            detail["question_id"] = None
+        if "accepted_answer_id" in item.keys():
+            detail["answer_id"] = item["accepted_answer_id"]
+        else:
+            detail["answer_id"] = None
+        if "title" in item.keys():
+            detail["title"] = item["title"]
+        else:
+            detail["title"] = None
+        if "link" in item.keys():
+            detail["link"] = item["link"]
+        else:
+            detail["link"] = None
+        if "last_edit_date" in item.keys():
+            detail["last_updated"] = item["last_edit_date"]
+        else:
+            detail["last_updated"] = None
+
+        faq.append(detail)
 
 
     return (response["has_more"], response["quota_remaining"])
@@ -74,13 +101,27 @@ def show_keys(json):
 def main():
     rq = -1
     has_more = True
-    while rq != 0 and has_more:
-        has_more, rq = get_tag_faq("css")
-        print(f"Processed page [{rparams.page}], Quota remaining [{rq}]")
-        rparams["page"] = rparams["page"] + 1
 
-    with open("faq.json", "a") as out:
-        json.dump({"details": faq}, out)
+    try:
+        while rq != 0 and has_more:
+            has_more, rq = get_tag_faq("css")
+            curr_page = rparams["page"]
+            print(f"Processed page [{curr_page}], Quota remaining [{rq}]")
+            rparams["page"] = rparams["page"] + 1
+
+        curr_page = rparams["page"]
+        print(f"\n\nData Collection Complete!\nPage Bookmark [{curr_page}]\nRemaining Quota [{rq}]")
+    except KeyboardInterrupt as k:
+        print("Program halted!")
+        print(f"\n\nData Collection Interrupted!\nPage Bookmark [{curr_page}]\nRemaining Quota [{rq}]")
+    except Exception as e:
+        print("Some error occured!")
+        print(e)
+        print(f"\n\nData Collection Interrupted!\nPage Bookmark [{curr_page}]\nRemaining Quota [{rq}]")
+    finally:
+        with open("faq.json", "a") as out:
+            json.dump({"details": faq}, out)
+        print("\nJSON dumped to file")
 
 
 if __name__ == '__main__':
