e1894b64be2aefe253fc3d5f057e777b9fa2ba4e
imtiyaazthompson <imtiyaaztom@gmail.com>
2021-03-08 15:17:19 +0200

    Figure out how to collect individual questions and answersin a batch format of 500 Calls to mitigate stalling. Need to keep track of the IDs used for a call

--- src/stackapi.py
+++ src/stackapi.py
@@ -2,6 +2,8 @@ import config
 import searchkey as sk
 import json
 import requests
+import time
+import traceback
 
 # Example API request: https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow
 
@@ -11,8 +13,9 @@ API = "https://api.stackexchange.com/2.2/"
 rparams = {
     "key": API_KEY,
     "site": "stackoverflow",
-    "page": 197,
+    "page": 1,
     "pagesize": 100,
+    "filter": "withbody",
 }
 
 # Hold all details from a call to the API
@@ -88,6 +91,27 @@ def get_tag_faq(tag):
     return (response["has_more"], response["quota_remaining"])
 
 
+def get_q(qid):
+    request_url = API + f"questions/{qid}?"
+    response = get(request_url)
+
+    if "items" in response.keys():
+        question = response["items"][0]["body"]
+        return (qid,question)
+    else:
+        return (-1, -1)
+
+def get_a(aid):
+    request_url = API + f"answers/{aid}?"
+    response = get(request_url)
+
+    if "items" in response.keys():
+        answer = response["items"][0]["body"]
+        return (aid,answer)
+    else:
+        return (-1, -1)
+
+
 def load_json():
     with open("faq.json", "r") as faq:
         loaded_json = json.load(faq)
@@ -98,7 +122,7 @@ def show_keys(json):
     print(json.keys())
 
 
-def main():
+def collect_faq():
     rq = -1
     has_more = True
 
@@ -119,9 +143,84 @@ def main():
         print(e)
         print(f"\n\nData Collection Interrupted!\nPage Bookmark [{curr_page}]\nRemaining Quota [{rq}]")
     finally:
-        with open("faq.json", "a") as out:
+        with open("faq2.json", "a") as out:
             json.dump({"details": faq}, out)
         print("\nJSON dumped to file")
+
+
+def collect_qa():
+    # load json
+    with open("faq.json", "r") as jfaq:
+        faqs = json.load(jfaq)
+
+    # Open file for reading encountered ids
+    f = open("enc.txt", "a").close()
+    f = open("enc.txt", "r")
+    processed = f.readlines()
+    for j in range(len(processed)): processed[j] = processed[j].strip()
+    f.close()
+
+    f = open("enc.txt", "a")
+    print(processed)
+    ids = []
+
+    try:
+        qa_bodies = []
+        i = 1
+        for qa in faqs["details"]:
+            if str(qa["question_id"]) in processed or str(qa["answer_id"]) in processed:
+                print("ID already encountered!")
+                continue
+
+            if qa["is_answered"]:
+                time.sleep(1)
+                qid, q = get_q(qa["question_id"])
+                aid, a = get_a(qa["answer_id"])
+
+                # Some API responses may not return any information
+                if (qid == -1) or (aid == -1):
+                    print("Missing information encountered!")
+                    continue
+
+                ids.append(qid)
+                ids.append(aid)
+
+                print(f"Processed QA [{i}]/[49300]")
+                i += 1
+
+                details = {
+                    "question_id": qid,
+                    "question": q,
+                    "answer_id": aid,
+                    "answer": a
+                }
+
+                qa_bodies.append(details)
+
+        print("QA Collection complete!")
+    except KeyboardInterrupt as k:
+        print("Program halted!")
+    except Exception as e:
+        print("Some error occured!")
+        traceback.print_exc(e)
+    finally:
+        print(f"Processed [{i}] QAs")
+        with open("qab.json", "a") as qab:
+            json.dump({"details": qa_bodies}, qab)
+            generate_encounter_ids(f, ids)
+        print("QA Collection successfully dumped!")
+
+
+def generate_encounter_ids(f, ids):
+    for id in ids:
+        f.write(f"{id}\n")
+
+    f.close()
+
+
+def main():
+     # collect_faq()
+     collect_qa()
 
 
 if __name__ == '__main__':
